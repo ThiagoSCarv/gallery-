c3720a74a4bcacce9e6b41eafe569ec0a014a30a
Thiago <thiagocarv4@gmail.com>
2026-04-14 13:03:12 -0300

    feat: listando as photos (componente PhotoList)

---
+++ src/contexts/photos/components/photos-list.tsx
@@ -0,0 +1,53 @@
+import type { Photo } from "../models/photo";
+import PhotoWidget from "./photo-widget";
+import Text from "../../../components/text";
+import Skeleton from "../../../components/skeleton";
+
+interface PhotosListProps {
+	photos: Photo[];
+	loading?: boolean;
+}
+
+export default function PhotosList({ photos, loading }: PhotosListProps) {
+	return (
+		<div className="space-y-6">
+			<Text
+				as="div"
+				variant="paragraph-large"
+				className="flex items-center justify-end text-accent-span "
+			>
+				Total:{" "}
+				{!loading ? (
+					<div>{photos.length}</div>
+				) : (
+					<Skeleton className="w-6 h-6" />
+				)}
+			</Text>
+			{!loading && photos?.length > 0 && (
+				<div className="grid grid-cols-5 gap-4">
+					{photos.map((photo) => {
+						return <PhotoWidget photo={photo} key={photo.id} />;
+					})}
+				</div>
+			)}
+			{loading && (
+				<div className="grid grid-cols-5 gap-4">
+					{Array.from({ length: 10 }).map((_, index) => {
+						return (
+							<PhotoWidget
+								key={`photo-loading-${index}`}
+								photo={{} as Photo}
+								loading
+							/>
+						);
+					})}
+				</div>
+			)}
+			{!loading && photos.length === 0 && (
+				<div className="flex justify-center items-center h-full">
+					<Text variant="paragraph-large">Nenhuma foto encontrada</Text>
+				</div>
+			)}
+		</div>
+	);
+}
--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,95 +1,64 @@
 import Container from "../components/container";
-import PhotoWidget from "../contexts/photos/components/photo-widget";
+import PhotosList from "../contexts/photos/components/photos-list";
+import type { Photo } from "../contexts/photos/models/photo";
+
+const listPhotos: Photo[] = [
+	{
+		id: "1",
+		title: "Olá mundo",
+		imageId: "portrait-tower.png",
+		albums: [
+			{ id: "3421", title: "Album 1" },
+			{ id: "421", title: "Album 2" },
+			{ id: "321", title: "Album 3" },
+		],
+	},
+	{
+		id: "2",
+		title: "Olá mundo",
+		imageId: "portrait-tower.png",
+		albums: [
+			{ id: "3421", title: "Album 1" },
+			{ id: "421", title: "Album 2" },
+			{ id: "321", title: "Album 3" },
+		],
+	},
+	{
+		id: "3",
+		title: "Olá mundo",
+		imageId: "portrait-tower.png",
+		albums: [
+			{ id: "3421", title: "Album 1" },
+			{ id: "421", title: "Album 2" },
+			{ id: "321", title: "Album 3" },
+		],
+	},
+	{
+		id: "12",
+		title: "Olá mundo",
+		imageId: "portrait-tower.png",
+		albums: [
+			{ id: "3421", title: "Album 1" },
+			{ id: "421", title: "Album 2" },
+			{ id: "321", title: "Album 3" },
+		],
+	},
+	{
+		id: "23",
+		title: "Olá mundo",
+		imageId: "portrait-tower.png",
+		albums: [
+			{ id: "3421", title: "Album 1" },
+			{ id: "421", title: "Album 2" },
+			{ id: "321", title: "Album 3" },
+		],
+	},
+];
 
 export default function PageHome() {
 	return (
 		<Container>
-			<div className="grid grid-cols-4">
-				<PhotoWidget
-					photo={{
-						id: "123",
-						title: "Ola Mundo",
-						imageId: "portrait-tower.png",
-						albums: [
-							{
-								id: "321",
-								title: "Album 2",
-							},
-							{
-								id: "421",
-								title: "Album 3",
-							},
-							{
-								id: "221",
-								title: "Album 1",
-							},
-						],
-					}}
-				/>
-				<PhotoWidget
-					photo={{
-						id: "123",
-						title: "Ola Mundo",
-						imageId: "portrait-tower.png",
-						albums: [
-							{
-								id: "321",
-								title: "Album 2",
-							},
-							{
-								id: "421",
-								title: "Album 3",
-							},
-							{
-								id: "221",
-								title: "Album 1",
-							},
-						],
-					}}
-				/>
-				<PhotoWidget
-					photo={{
-						id: "123",
-						title: "Ola Mundo",
-						imageId: "portrait-tower.png",
-						albums: [
-							{
-								id: "321",
-								title: "Album 2",
-							},
-							{
-								id: "421",
-								title: "Album 3",
-							},
-							{
-								id: "221",
-								title: "Album 1",
-							},
-						],
-					}}
-				/>
-				<PhotoWidget
-					photo={{
-						id: "123",
-						title: "Ola Mundo",
-						imageId: "portrait-tower.png",
-						albums: [
-							{
-								id: "321",
-								title: "Album 2",
-							},
-							{
-								id: "421",
-								title: "Album 3",
-							},
-							{
-								id: "221",
-								title: "Album 1",
-							},
-						],
-					}}
-				/>
-			</div>
+			<PhotosList photos={listPhotos} />
 		</Container>
 	);
 }
