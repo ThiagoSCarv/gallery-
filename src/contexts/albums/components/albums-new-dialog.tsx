939f33f5419bbbc1de8c0c7d1a47050589896876
Thiago <thiagocarv4@gmail.com>
2026-04-17 17:41:53 -0300

    feat: retornando lista de fotos

--- src/contexts/albums/components/albums-new-dialog.tsx
+++ src/contexts/albums/components/albums-new-dialog.tsx
@@ -15,65 +15,14 @@ import type { Photo } from "../../photos/models/photo";
 import SelectCheckboxIllustration from "../../../assets/images/select-checkbox.svg?react";
 import Skeleton from "../../../components/skeleton";
 import PhotoImageSelectable from "../../photos/components/photo-image-selectable";
+import usePhotos from "../../photos/hooks/usePhotos";
 
 interface AlbumNewDialogProps {
 	trigger: React.ReactNode;
 }
 
 export default function AlbumNewDialog({ trigger }: AlbumNewDialogProps) {
-	const photos: Photo[] = [
-		{
-			id: "1",
-			title: "Olá mundo",
-			imageId: "portrait-tower.png",
-			albums: [
-				{ id: "3421", title: "Album 1" },
-				{ id: "421", title: "Album 2" },
-				{ id: "321", title: "Album 3" },
-			],
-		},
-		{
-			id: "2",
-			title: "Olá mundo",
-			imageId: "portrait-tower.png",
-			albums: [
-				{ id: "3421", title: "Album 1" },
-				{ id: "421", title: "Album 2" },
-				{ id: "321", title: "Album 3" },
-			],
-		},
-		{
-			id: "3",
-			title: "Olá mundo",
-			imageId: "portrait-tower.png",
-			albums: [
-				{ id: "3421", title: "Album 1" },
-				{ id: "421", title: "Album 2" },
-				{ id: "321", title: "Album 3" },
-			],
-		},
-		{
-			id: "12",
-			title: "Olá mundo",
-			imageId: "portrait-tower.png",
-			albums: [
-				{ id: "3421", title: "Album 1" },
-				{ id: "421", title: "Album 2" },
-				{ id: "321", title: "Album 3" },
-			],
-		},
-		{
-			id: "23",
-			title: "Olá mundo",
-			imageId: "portrait-tower.png",
-			albums: [
-				{ id: "3421", title: "Album 1" },
-				{ id: "421", title: "Album 2" },
-				{ id: "321", title: "Album 3" },
-			],
-		},
-	];
-	const isLoadingPhotos = false;
+	const { photos, isLoadingPhotos } = usePhotos();
 
 	function handleTogglePhoto(selected: boolean, photoId: string) {
 		console.log(selected, photoId);
@@ -95,7 +44,7 @@ export default function AlbumNewDialog({ trigger }: AlbumNewDialogProps) {
 								{photos.map((photo) => (
 									<PhotoImageSelectable
 										key={photo.id}
-										src={`/images/${photo.imageId}`}
+										src={`${import.meta.env.VITE_IMAGES_URL}/${photo.imageId}`}
 										title={photo.title}
 										imageClassName="w-20 h-20"
 										onSelectImage={(selected) =>
--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -2,63 +2,11 @@ import Container from "../components/container";
 import AlbumsFilter from "../contexts/albums/components/albums-filter";
 import useAlbums from "../contexts/albums/hooks/use-albums";
 import PhotosList from "../contexts/photos/components/photos-list";
-import type { Photo } from "../contexts/photos/models/photo";
-
-const listPhotos: Photo[] = [
-	{
-		id: "1",
-		title: "Olá mundo",
-		imageId: "portrait-tower.png",
-		albums: [
-			{ id: "3421", title: "Album 1" },
-			{ id: "421", title: "Album 2" },
-			{ id: "321", title: "Album 3" },
-		],
-	},
-	{
-		id: "2",
-		title: "Olá mundo",
-		imageId: "portrait-tower.png",
-		albums: [
-			{ id: "3421", title: "Album 1" },
-			{ id: "421", title: "Album 2" },
-			{ id: "321", title: "Album 3" },
-		],
-	},
-	{
-		id: "3",
-		title: "Olá mundo",
-		imageId: "portrait-tower.png",
-		albums: [
-			{ id: "3421", title: "Album 1" },
-			{ id: "421", title: "Album 2" },
-			{ id: "321", title: "Album 3" },
-		],
-	},
-	{
-		id: "12",
-		title: "Olá mundo",
-		imageId: "portrait-tower.png",
-		albums: [
-			{ id: "3421", title: "Album 1" },
-			{ id: "421", title: "Album 2" },
-			{ id: "321", title: "Album 3" },
-		],
-	},
-	{
-		id: "23",
-		title: "Olá mundo",
-		imageId: "portrait-tower.png",
-		albums: [
-			{ id: "3421", title: "Album 1" },
-			{ id: "421", title: "Album 2" },
-			{ id: "321", title: "Album 3" },
-		],
-	},
-];
+import usePhotos from "../contexts/photos/hooks/usePhotos";
 
 export default function PageHome() {
 	const { albums, isLoadingAlbums } = useAlbums();
+	const { photos, isLoadingPhotos } = usePhotos();
 
 	return (
 		<Container>
@@ -67,7 +15,7 @@ export default function PageHome() {
 				loading={isLoadingAlbums}
 				className="mb-9"
 			/>
-			<PhotosList photos={listPhotos} />
+			<PhotosList photos={photos} loading={isLoadingPhotos} />
 		</Container>
 	);
 }
